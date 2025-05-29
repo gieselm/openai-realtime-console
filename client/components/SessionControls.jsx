8d1c6efdf3b1dbcdd5e07adb5b54a0008cb70e73
gieselm <max.gieselmann@posteo.de>
2025-05-29 18:55:32 +0200

    Add debug logging for song recommendations

--- client/components/SessionControls.jsx
+++ client/components/SessionControls.jsx
@@ -61,6 +61,7 @@ export default function SessionControls({
       ) : (
         <div className="flex items-center justify-center w-full">
           <p className="text-gray-500">Connecting...</p>
+          
         </div>
       )}
     </div>
